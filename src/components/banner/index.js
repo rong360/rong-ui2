66d5c866cf743aa45b866c031a974943de4c7e77
jay200212003 <397602528@qq.com>
2020-11-10 11:17:26 +0800

    delete banner

--- src/components/banner/index.js
+++
@@ -1,9 +0,0 @@
-import Banner from './banner'
-import BannerItem from './banner-item'
-
-Banner.install = function (Vue) {
-  Vue.component(Banner.name, Banner);
-  Vue.component(BannerItem.name, BannerItem);
-}
-
-export default Banner
