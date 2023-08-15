6df7d9949ff0ad554d54788a45440a7c9833a9fc
liwenjing <liwenjng@rong360.com>
2023-08-15 17:25:39 +0800

    update release

--- release.sh
+++ release.sh
@@ -75,7 +75,10 @@ cd release
 
 #npm publish
 
+# 将 dist-example 目录发布到 gh-pages 分支， 发布之前需要先将 dist-example 目录提交到 master 分支
+# git subtree push --prefix dist-example origin gh-pages
 
-npm link
-cd ../
-npm link rong-ui2
+
+# npm link
+# cd ../
+# npm link rong-ui2
